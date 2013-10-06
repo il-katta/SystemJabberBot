471fb52cc7023a8fc499aed7f5c00caa669fb04e
il-katta <ilkatta88@gmail.com>
2013-10-06 18:51:59 +0200

    up

--- SystemUtils.py
+++ SystemUtils.py
@@ -7,44 +7,64 @@ import os
 from shlex import split
 from os import getcwd, chdir
 from subprocess import Popen,PIPE
-import cStringIO
+#import cStringIO
 
 class SystemUtils(object):
-    __cwd=None
+    _cwd=None
 
     def __init__(self):
-        self.__cwd=getcwd()
+        self._cwd=getcwd()
 
-    def __execmd(self,cmd):
-        def split_cmd_pipe(pcmd):
-            #i = args.index('|')
-            i = (len(args) - 1) - args[::-1].index('|')
-            l = ' '.join(args[:i])
-            r = args[i+1:]
+    def _execmd(self,cmd):
+        def which_cmd(args):
+            args[0] = self.which(args[0]) or args[0]
+            return args
+
+        def split_cmd(pcmd,s):
+            i = (len(args) - 1) - args[::-1].index(s)
+            l = which_cmd( args[:i] )
+            r = which_cmd( args[i+1:] )
             return l,r
 
         args=split(cmd)
+        
+
         if '|' in args:
-            l , r = slit_cmd_pipe(args)            
-            return Popen(r, stdin=self.__execmd(l),stdout=PIPE).stdout
-        else:
-            return  Popen(args, stdout=PIPE).stdout
+            l , r = split_cmd(args,'|')
+            #p = Popen(r, stdin=self._execmd( ' '.join(l) ),stdout=PIPE)
+            retl , pin = self._execmd( ' '.join(l) )
+            p = Popen(r, stdout=PIPE, stdin=PIPE) 
+            stdout, stderr = p.communicate(input=pin)
+            return p.wait() , stdout
+        
+        if '&&' in args:
+            l , r = split_cmd(args,'&&')
+            retcl, stdoutl = self._execmd( ' '.join(l) )
+            if retcl is 0:
+                retcr , stdoutr = self._execmd( ' '.join(r) )
+                return retcr , "%s%s" % (stdoutl,stdoutr) 
+            return retcl, stdoutl
+        
+        p = Popen(args, stdout=PIPE)
+        retc = p.wait()
+        stdout = p.stdout.read()
+        return retc , stdout
 
     def execmd(self,cmd):
         rcwd=getcwd()
-        chdir(self.__cwd)
-        ret = self.__execmd(cmd)
+        chdir(self._cwd)
+        code, out = self._execmd(cmd)
         chdir(rcwd)
-        return ret.read()
+        return out
 
     def cd(self,path):
         try:
-            self.__cwd=path;
+            self._cwd=path;
         except Exception:
             pass
 
     def pwd(self):
-        return self.__cwd
+        return self._cwd
 
     @staticmethod
     def cat(sfile):
